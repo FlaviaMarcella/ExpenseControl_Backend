93f355a30e7ca9f69a7f380701dc4bf0fbefc6cd
Flávia Gonçalves <flavia.marcella@alunos.ifsuldeminas.edu.br>
2026-07-27 09:30:09 -0300

    Rename AppDbContext and update namespace; add Transactions DbSet

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseControl.Api.Model.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseControl.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<People> Peoples => Set<People>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+}
