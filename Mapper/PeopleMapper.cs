f3aa336ef2922d49a9a0a55c6d363e9f7596532b
Flávia Gonçalves <flavia.marcella@alunos.ifsuldeminas.edu.br>
2026-07-27 09:49:50 -0300

    Add mappers for People and Transaction to convert between entities and DTOs

---
+++ Mapper/PeopleMapper.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseControl.Api.Dto;
+using ExpenseControl.Api.Model.Entity;
+
+namespace ExpenseControl.Api.Mapper;
+
+public class PeopleMapper
+{
+    public PeopleDto MapToDto(People people)
+    {
+        return new PeopleDto(people.Id, people.Name, people.LastName, people.Age, people.Relationship, people.Email,
+            people.Phone);
+    }
+
+    public People MapToEntity(PeopleDto peopleDto)
+    {
+        return new People
+        {
+            Id = peopleDto.Id,
+            Name = peopleDto.Name,
+            LastName = peopleDto.LastName,
+            Age = peopleDto.Age,
+            Relationship = peopleDto.Relationship,
+            Email = peopleDto.Email,
+            Phone = peopleDto.Phone
+        };
+    }
+}
---
+++ Mapper/TransactionMapper.cs
@@ -0,0 +1,39 @@
+﻿using ExpenseControl.Api.Dto;
+using ExpenseControl.Api.Model.Entity;
+using ExpenseControl.Api.Model.Enums;
+
+namespace ExpenseControl.Api.Mapper;
+
+public class TransactionMapper
+{
+    public TransactionDto MapToDto(Transaction transaction)
+    {
+        var peopleMapper = new PeopleMapper();
+        var peopleDto = peopleMapper.MapToDto(transaction.People);
+
+        return new TransactionDto(
+            transaction.Id,
+            transaction.Description,
+            transaction.Amount,
+            transaction.Date,
+            transaction.Type.ToString(),
+            peopleDto
+        );
+    }
+
+    public Transaction MapToEntity(TransactionDto transactionDto)
+    {
+        var peopleMapper = new PeopleMapper();
+        var people = peopleMapper.MapToEntity(transactionDto.People);
+
+        return new Transaction
+        {
+            Id = transactionDto.Id,
+            Amount = transactionDto.Amount,
+            Date = transactionDto.Date,
+            Description = transactionDto.Description,
+            Type = Enum.Parse<TypeTransaction>(transactionDto.Type),
+            People = people
+        };
+    }
+}
