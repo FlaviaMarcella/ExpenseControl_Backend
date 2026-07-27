a3af036af81d1ca2c7a81ddddc255109c4fc9f61
Flávia Gonçalves <flavia.marcella@alunos.ifsuldeminas.edu.br>
2026-07-26 21:33:52 -0300

    add People class and Relationship enum with descriptions

---
+++ People.cs
@@ -0,0 +1,11 @@
+﻿namespace ExpenseControl.Api;
+
+public class People
+{
+    private int _id {get; set;}
+    private string _name {get; set;}
+    private int _age {get; set;}
+    private string _relationship {get; set;} 
+    private string _email {get; set;}
+    private string _phone {get; set;}
+}
---
+++ Relationship.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace ExpenseControl.Api;
+
+public enum Relationship
+{
+    [Description("Pai")]
+    Father,
+    [Description("Mãe")]
+    Mother,
+    [Description("Filho(a)")]
+    Child,
+    [Description("Irmão(ã)")]
+    Sibling,
+    [Description("Avô(ó)")]
+    Grandparent,
+    [Description("Tio(a)")]
+    UncleAunt,
+    [Description("Primo(a)")]
+    Cousin,
+    [Description("Amigo(a)")]
+    Friend,
+    [Description("Outro")]
+    Other
+}
+
+public static class RelationshipExtensions {
+    public static string getDescription(this Enum value) {
+        FieldInfo field = value.GetType().GetField(value.ToString());
+        var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
+        return attribute?.Description ?? value.ToString();
+    }
+}
