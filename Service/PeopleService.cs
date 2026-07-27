d8cc6debab2974ac6d73572a874c1333cc212782
Flávia Gonçalves <flavia.marcella@alunos.ifsuldeminas.edu.br>
2026-07-27 11:30:11 -0300

    Add PeopleService and TransactionService with CRUD operations

---
+++ Service/PeopleService.cs
@@ -0,0 +1,89 @@
+﻿using ExpenseControl.Api.Data;
+using ExpenseControl.Api.Dto;
+using ExpenseControl.Api.Mapper;
+using ExpenseControl.Api.Model.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseControl.Api.Service;
+
+public class PeopleService(
+    AppDbContext context,
+    ILogger<PeopleService> logger,
+    PeopleMapper peopleMapper,
+    TransactionService transactionService) : IPeopleService
+{
+    public async Task<IEnumerable<PeopleDto>> GetAllAsync()
+    {
+        var peoples = await context.Peoples.ToListAsync();
+        return peoples.Select(p => peopleMapper.MapToDto(p));
+    }
+
+    public async Task<PeopleDto?> GetByIdAsync(int id)
+    {
+        var people = await context.Peoples.FindAsync(id);
+        if (people == null)
+        {
+            return null;
+        }
+
+        logger.LogInformation("Retrieved people with ID {Id}: {@People}", id, people);
+        return peopleMapper.MapToDto(people);
+    }
+
+    public async Task<PeopleDto> CreateAsync(PeopleDto peopleDto)
+    {
+        var people = peopleMapper.MapToEntity(peopleDto);
+
+        context.Peoples.Add(people);
+        await context.SaveChangesAsync();
+        logger.LogInformation("Created new people: {@People}", people);
+        return peopleMapper.MapToDto(people);
+    }
+
+    public async Task<PeopleDto?> UpdateAsync(int id, PeopleDto peopleDto)
+    {
+        var people = await context.Peoples.FindAsync(id);
+        if (people == null)
+        {
+            return null;
+        }
+
+        people.Name = peopleDto.Name;
+        people.LastName = peopleDto.LastName;
+        people.Age = peopleDto.Age;
+        people.Relationship = peopleDto.Relationship;
+        people.Email = peopleDto.Email;
+        people.Phone = peopleDto.Phone;
+
+        await context.SaveChangesAsync();
+        logger.LogInformation("Updated people with ID {Id}: {@People}", id, people);
+        return peopleMapper.MapToDto(people);
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var people = await context.Peoples.FindAsync(id);
+        if (people == null)
+        {
+            return false;
+        }
+
+        await transactionService.DeleteAllByPeopleIdAsync(id);
+
+        context.Peoples.Remove(people);
+        await context.SaveChangesAsync();
+        logger.LogInformation("Deleted people with ID {Id}: {@People}", id, people);
+        return true;
+    }
+
+
+    public bool AgeVerified(int age)
+    {
+        if (age < 18)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Service/TransactionService.cs
@@ -0,0 +1,119 @@
+﻿using ExpenseControl.Api.Data;
+using ExpenseControl.Api.Dto;
+using ExpenseControl.Api.Mapper;
+using ExpenseControl.Api.Model.Enums;
+using ExpenseControl.Api.Model.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseControl.Api.Service;
+
+public class TransactionService(
+    AppDbContext context,
+    PeopleService peopleService,
+    ILogger<TransactionService> logger,
+    TransactionMapper transactionMapper) : ITransactionService
+{
+    public async Task<IEnumerable<TransactionDto>> GetAllAsync()
+    {
+        var transactions = await context.Transactions.ToListAsync();
+        return transactions.Select(transactionMapper.MapToDto);
+    }
+
+    public async Task<IEnumerable<TransactionDto>> GetByPeopleIdAsync(int peopleId)
+    {
+        var peopleExists = await peopleService.GetByIdAsync(peopleId);
+        if (peopleExists == null)
+        {
+            throw new InvalidOperationException($"People with ID {peopleId} does not exist.");
+        }
+
+        var transactions = await context.Transactions.Where(t => t.People.Id == peopleId).ToListAsync();
+        return transactions.Select(transactionMapper.MapToDto);
+    }
+
+    public async Task<TransactionDto?> GetByIdAsync(int id)
+    {
+        var transaction = await context.Transactions.FindAsync(id);
+        if (transaction == null)
+        {
+            return null;
+        }
+
+        logger.LogInformation("Retrieved transaction with ID {Id}: {@Transaction}", id, transaction);
+        return transactionMapper.MapToDto(transaction);
+    }
+
+    public async Task<TransactionDto> CreateAsync(TransactionDto transactionDto)
+    {
+        var transaction = transactionMapper.MapToEntity(transactionDto);
+        context.Transactions.Add(transaction);
+
+        if (peopleService.AgeVerified(transaction.People.Age)! && transaction.Type == TypeTransaction.Receive)
+        {
+            throw new InvalidOperationException("People under 18 years old cannot create a receive transaction.");
+        }
+
+        await context.SaveChangesAsync();
+        logger.LogInformation("Created new transaction: {@Transaction}", transaction);
+        return transactionMapper.MapToDto(transaction);
+    }
+
+    public async Task<TransactionDto?> UpdateAsync(int id, TransactionDto transactionDto)
+    {
+        var transaction = await context.Transactions.FindAsync(id);
+        if (transaction == null)
+        {
+            return null;
+        }
+
+        var peopleExists = await peopleService.GetByIdAsync(transactionDto.People.Id);
+        if (peopleExists == null)
+        {
+            throw new InvalidOperationException($"People with ID {transactionDto.People.Id} does not exist.");
+        }
+
+        transaction.Description = transactionDto.Description;
+        transaction.Amount = transactionDto.Amount;
+        transaction.Date = transactionDto.Date;
+        transaction.Type = Enum.Parse<TypeTransaction>(transactionDto.Type);
+        transaction.People = (await context.Peoples.FindAsync(transactionDto.People.Id))!;
+        await context.SaveChangesAsync();
+        logger.LogInformation("Updated transaction with ID {Id}: {@Transaction}", id, transaction);
+        return transactionMapper.MapToDto(transaction);
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var transaction = await context.Transactions.FindAsync(id);
+        if (transaction == null)
+        {
+            return false;
+        }
+
+        context.Transactions.Remove(transaction);
+        await context.SaveChangesAsync();
+        logger.LogInformation("Deleted transaction with ID {Id}: {@Transaction}", id, transaction);
+        return true;
+    }
+
+
+    public async Task<bool> DeleteAllByPeopleIdAsync(int peopleId)
+    {
+        var peopleExists = await peopleService.GetByIdAsync(peopleId);
+        if (peopleExists == null)
+        {
+            throw new InvalidOperationException($"People with ID {peopleId} does not exist.");
+        }
+
+        var transactions = await context.Transactions.Where(t => t.People.Id == peopleId).ToListAsync();
+        if (!transactions.Any())
+        {
+            return false;
+        }
+
+        context.Transactions.RemoveRange(transactions);
+        await context.SaveChangesAsync();
+        logger.LogInformation("Deleted all transactions for people with ID {PeopleId}", peopleId);
+        return true;
+    }
+}
