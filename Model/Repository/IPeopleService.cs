5dc9a2d8fe0601afecff97d79846738394fa2e61
Flávia Gonçalves <flavia.marcella@alunos.ifsuldeminas.edu.br>
2026-07-27 11:30:02 -0300

    Add service interfaces for People and Transaction with CRUD operations

---
+++ Model/Repository/IPeopleService.cs
@@ -0,0 +1,12 @@
+﻿using ExpenseControl.Api.Dto;
+
+namespace ExpenseControl.Api.Model.Repository;
+
+public interface IPeopleService
+{
+    Task<IEnumerable<PeopleDto>> GetAllAsync();
+    Task<PeopleDto?> GetByIdAsync(int id);
+    Task<PeopleDto> CreateAsync(PeopleDto peopleDto);
+    Task<PeopleDto?> UpdateAsync(int id, PeopleDto peopleDto);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ Model/Repository/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseControl.Api.Dto;
+
+namespace ExpenseControl.Api.Model.Repository;
+
+public interface ITransactionService
+{
+    Task<IEnumerable<TransactionDto>> GetAllAsync();
+    Task<IEnumerable<TransactionDto>> GetByPeopleIdAsync(int peopleId);
+    Task<TransactionDto?> GetByIdAsync(int id);
+    Task<TransactionDto> CreateAsync(TransactionDto transactionDto);
+    Task<TransactionDto?> UpdateAsync(int id, TransactionDto transactionDto);
+    Task<bool> DeleteAsync(int id);
+    Task<bool> DeleteAllByPeopleIdAsync(int peopleId);
+}
